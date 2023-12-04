5e152eb5f148794f9a75c0c26facad3d0d7723bf
manoharn725 <manoharnmanu725@gmail.com>
2023-12-04 12:09:23 +0530

    added fetchUsers & addUser in thunks and Skeleton page created in components

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,51 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers, addUser } from "../store";
+import Button from './Button';
+import Skeleton from "./Skeleton";
+
 function UsersList (){
+    const dispatch = useDispatch();
+    const {isLoading, data, error} = useSelector((state)=>{
+        return state.users; // { data: [], isLoading: false, error: null }
+    });
+
+    useEffect(()=>{
+        dispatch(fetchUsers());
+    }, [dispatch]);
+
+    const handleUserAdd = ()=> {
+        dispatch(addUser());
+    }
+    
+    if(isLoading){
+        return (
+            <Skeleton times={10} className='h-10 w-full' />
+        )
+    }
+    if(error){
+        return (
+            <div>Error fetching data..</div>
+        )
+    }
+   
+    const renderedUsers = data.map((user)=>{
+        return <div key={user.id} className="mb-2 border rounded">
+            <div className="flex p-2 justify-between items-center cursor-pointer">
+            {user.id} {user.name}
+            </div>
+        </div>
+    })
     return(
-        <div>UsersList</div>
+        <div>
+            <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={handleUserAdd}>
+            + Add User
+        </Button>
+            </div>
+            {renderedUsers}
+            </div>
     )
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
 
-const store = configureStore({
+export const store = configureStore({
     reducer: {
         users: usersReducer,
     }
 })
 
-export { store };
+export * from './thunks/fetchUsers';
+export * from './thunks/addUser';
