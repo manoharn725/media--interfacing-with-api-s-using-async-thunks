cd03e0beec427a3509ffa2143dd1559bb52c2a40
manoharn725 <manoharnmanu725@gmail.com>
2023-12-07 16:42:08 +0530

    Handling Errors with user creation

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,52 +1,66 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
-import Button from './Button';
+import Button from "./Button";
 import Skeleton from "./Skeleton";
 
-function UsersList (){
-    const dispatch = useDispatch();
-    const {isLoading, data, error} = useSelector((state)=>{
-        return state.users; // { data: [], isLoading: false, error: null }
-    });
+function UsersList() {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
+  const [isCreatingUser, setIsCreatingUser] = useState(false);
+  const [creatingUserError, setCreatingUserError] = useState(null);
 
-    useEffect(()=>{
-        dispatch(fetchUsers());
-    }, [dispatch]);
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => {
+    return state.users; // { data: [], isLoading: false, error: null }
+  });
 
-    const handleUserAdd = ()=> {
-        dispatch(addUser());
-    }
-    
-    if(isLoading){
-        return (
-            <Skeleton times={10} className='h-10 w-full' />
-        )
-    }
-    if(error){
-        return (
-            <div>Error fetching data..</div>
-        )
-    }
-   
-    const renderedUsers = data.map((user)=>{
-        return <div key={user.id} className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
-            {user.id} {user.name}
-            </div>
+  useEffect(() => {
+    setIsLoadingUsers(true);
+    dispatch(fetchUsers())
+      .unwrap()
+      .catch((err) => setLoadingUsersError(err))
+      .finally(() => setIsLoadingUsers(false));
+  }, [dispatch]);
+
+  const handleUserAdd = () => {
+    dispatch(addUser())
+      .unwrap()
+      .catch((err) => setCreatingUserError(err))
+      .finally(() => setIsCreatingUser(false));
+  };
+
+  if (isLoadingUsers) {
+    return <Skeleton times={10} className="h-10 w-full" />;
+  }
+  if (loadingUsersError) {
+    return <div>Error fetching data..</div>;
+  }
+
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.id} {user.name}
         </div>
-    })
-    return(
-        <div>
-            <div className="flex flex-row justify-between m-3">
+      </div>
+    );
+  });
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        <Button onClick={handleUserAdd}>
-            + Add User
-        </Button>
-            </div>
-            {renderedUsers}
-            </div>
-    )
+        {
+        isCreatingUser ? (
+          "Creatinf User..."
+        ) : (
+          <Button onClick={handleUserAdd}>+ Add User</Button>
+        )}
+        {creatingUserError && "Error creating user..."}
+      </div>
+      {renderedUsers}
+    </div>
+  );
 }
 
 export default UsersList;
