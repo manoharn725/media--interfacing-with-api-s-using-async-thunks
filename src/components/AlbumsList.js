37a69fd43ca115d66ae262884a5b4f1f004f05d7
manoharn725 <manoharnmanu725@gmail.com>
2023-12-09 17:14:33 +0530

    using ReduxToolKit Query Api

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,9 +1,33 @@
-function AlbumsList({user}){
-    return(
-        <div>
-            Albums for {user.name}
-        </div>
-    )
+import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+// import Button from "./Button";
+
+function AlbumsList({ user }) {
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of Photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
