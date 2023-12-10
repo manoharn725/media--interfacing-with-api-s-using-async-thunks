0cd5e6a21a43d315f1886ab2bf5c4e4249817dfc
manoharn725 <manoharnmanu725@gmail.com>
2023-12-10 19:00:37 +0530

    Adding delete icon when ever over on the image

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,8 +1,19 @@
+import { GoTrashcan } from "react-icons/go";
+import { useRemovePhotoMutation } from "../store";
+
 function PhotosListItem({ photo }) {
-  console.log(photo);
+  const [removePhoto] = useRemovePhotoMutation();
+    
+  const handleRemovePhoto = () => {
+        removePhoto(photo);
+    }
+
   return (
-    <div>
+    <div onClick={handleRemovePhoto} className="relative cursor-pointer m-2">
       <img className="h-20 w-20" src={photo.url} alt={photo.id} />
+      <div className="absolute inset-0 flex  items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-70">
+        <GoTrashcan  className="text-3xl"/>
+      </div>
     </div>
   );
 }
