e5a2680af6896d92c91bb1a63fa7028cb6152c31
manoharn725 <manoharnmanu725@gmail.com>
2023-12-10 08:40:49 +0530

    updated AlbumList

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,10 +1,15 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
 import Skeleton from "./Skeleton";
 import ExpandablePanel from "./ExpandablePanel";
-// import Button from "./Button";
+import Button from "./Button";
 
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
 
   let content;
   if (isLoading) {
@@ -14,17 +19,25 @@ function AlbumsList({ user }) {
   } else {
     content = data.map((album) => {
       const header = <div>{album.title}</div>;
+
       return (
         <ExpandablePanel key={album.id} header={header}>
-          List of Photos in the album
+          List of Photos in the album.
         </ExpandablePanel>
       );
+
     });
   }
 
+  
   return (
     <div>
-      <div>Albums for {user.name}</div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">
+        Albums for {user.name}
+        </h3>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>+ Add Album</Button>
+      </div>
       <div>{content}</div>
     </div>
   );
